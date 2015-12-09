b16cf0354b4c4ad81421f0b2ca92ea5c7547384e
idinkova <inna.dinkova@gmail.com>
2015-12-09 10:52:03 +0200

    Update Player.cs

--- RolePlayingGame/Characters/Players/Player.cs
+++ RolePlayingGame/Characters/Players/Player.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace RolePlayingGame.Characters
 {
     using Interfaces;
-    public abstract class Player : Character, IAttack, IMovable
+    public class Player : Character, IAttack, IMovable
     {
         public Player(string id, int x, int y, int healthPoints, int defensePoints, int attackPoints)
             : base(id, x, y, healthPoints, defensePoints, attackPoints)
